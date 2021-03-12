4faca8669212c4eb8bc09dabfc2c5b8f7e9c9deb
juliet879 <gisembajuliett@gmail.com>
2021-03-12 11:47:37 +0300

    add last commit

--- src/strings.kt
+++ src/strings.kt
@@ -1,9 +1,10 @@
 fun main() {
     akir()
-    println(person("Juliet",21))
+    println(person("Juliet", 21))
     println(long("The power of a Vision Board"))
-    println(condition("Juliet"))
+    condition("Beth")
 }
+
 fun akir() {
     var word = "Akirachix"
     var a = word[0]
@@ -12,18 +13,18 @@ fun akir() {
     println("$a$b$c")
 }
 
-fun person(name:String,age:Int):String {
+fun person(name: String, age: Int): String {
     return "Hi my name is $name and I am $age years old ."
 }
 
-fun long(word:String):Int {
+fun long(word: String): Int {
     return word.length
 }
 
-fun condition(jina:String) {
-    if (jina == "Juliet"){
-        print("That's me!")
-    }else{
-        print("I dont know who that is")
+fun condition(jina: String) {
+    if (jina == "Juliet") {
+        println("That's me!")
+    } else {
+        println("I dont know who that is.")
     }
 }
